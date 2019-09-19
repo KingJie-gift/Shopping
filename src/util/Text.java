e63b3b37d17e76501fdc59796a390455bbaccf41
2479744143@qq.com <2479744143@qq.com>
2019-09-20 00:59:45 +0800

    abc

--- src/util/Text.java
+++ src/util/Text.java
@@ -17,7 +17,7 @@ import java.util.Random;
 
 public class Text extends BaseDao {
     public static void main(String[] args) {
-        System.out.println("anroid");
+        System.out.println("java");
 //        Text text = new Text();
 //        List<Integer> list  = text.getIntegers();
 //        for(Integer integer:list){
